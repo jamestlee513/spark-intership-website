53c2902e1bf135a4af0a55caee18b9d86584c399
James Lee <jamestlee513@gmail.com>
2023-07-19 20:08:40 -0700

    Adding github-test folder and minor updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 function App() {
   return (
