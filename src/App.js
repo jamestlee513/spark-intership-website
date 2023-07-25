dc14817434d16687e88a9706cf679e69f4a861de
Andrew <ngand000@uw.edu>
2023-07-25 15:03:12 -0700

    Created working pages with links

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import ApplicationPage from "./pages/ApplicationPages/applicationPage";
+import HomePage from "./pages/HomePage/homePage";
+import ReviewPage from "./pages/ApplicationPages/reviewPage";
+import SubmitPage from "./pages/ApplicationPages/submitPage";
 
 // This is the app (this comment is very useful)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <div className="App">  
+    <Routes>  
+        <Route exact path='/' element={ <HomePage />}></Route>  
+        <Route exact path='/application' element={<ApplicationPage />}></Route>  
+    </Routes>
+    </div>  
+    </BrowserRouter>   
+
+    // <div className="App">
+    //   <ul>  
+    //   <li>  
+    //     <Link to="/">Home</Link>  
+    //   </li>  
+    //   <li>  
+    //     <Link to="/application">Application</Link>  
+    //   </li>  
+    // </ul>  
+    //   <BrowserRouter>
+    //     <Routes>
+    //       // Once you make your page, replace this component
+    //       <Route strict exact path="/" component={ ApplicationPage }/>
+    //       // Application Page
+    //       <Route exact path='/application' component ={ ApplicationPage}/>  
+    //       <Route path="/reviews" component={ ReviewPage }/>
+    //       <Route path="/submit" component={ SubmitPage }/>
+    //     </Routes>
+    //   </BrowserRouter>
+    // </div>
   );
 }
 
--- src/pages/ApplicationPages/submitPage.js
+++ src/pages/ApplicationPages/submitPage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Container } from "react-bootstrap"
 
 export default function SubmitPage() {
     return (
