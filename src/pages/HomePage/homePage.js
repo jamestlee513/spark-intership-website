20dc3588be9c710e114c39e7143de9292c899598
zylim1128 <zylim@uw.edu>
2023-07-28 00:16:48 -0700

    Added a Footer with SignOut option for the homePage

--- src/pages/HomePage/homePage.js
+++ src/pages/HomePage/homePage.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import Footer from '../../ui-components/Footer';
 
 export default function HomePage() {
     return (
-     <ul className="App-header">  
-       <li>  
-         <Link to="/application">Application Page</Link>  
-       </li>  
-     </ul> 
+     <><ul className="App-header">
+        <li>
+          <Link to="/application">Application Page</Link>
+        </li>
+      </ul>
+      <div className="App">
+          <Footer />
+      </div></>
+
     )
 }
