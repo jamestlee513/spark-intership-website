a2b2721d7195dd1dbc1fab83b1077aa031f5749b
zylim1128 <zylim@uw.edu>
2023-07-28 13:44:04 -0700

    Added a structure profile page

--- src/pages/ProfilePage/profilePage.js
+++ src/pages/ProfilePage/profilePage.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 const ProfilePage = () => {
   return (
-    <>
-    <div>
-      <h1>Profile</h1>
+    <div style={{ display: 'flex', alignItems: 'center', padding: '20px' }}>
+      <div style={{ flex: 1, flexDirection: 'column' }}>
+        <h2>Username</h2>
+        <p>Description here</p>
+        
+        {/* Education Level */}
+        <h3>Education level</h3>
+      </div>
+
+      <img
+        src=''
+        alt="user's pic"
+        style={{ width: '100px', height: '100px', margin: '50px'}}
+      />
     </div>
-    </>
   );
-}
+};
 
 export default ProfilePage;
