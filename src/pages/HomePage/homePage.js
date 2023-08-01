a5dd1fc8a8e9a0bf1d306d2ebcd60e9f75ab25fa
zylim1128 <zylim@uw.edu>
2023-08-01 16:43:10 -0700

    redesign home page with header and basic printout

--- src/pages/HomePage/homePage.js
+++ src/pages/HomePage/homePage.js
@@ -1,21 +1,59 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import Header from '../../ui-components/Header';
 import Footer from '../../ui-components/Footer';
+import SparkLogo from '../../images/SparkLogo-Temp.png';
 
 export default function HomePage() {
     return (
      <>
-     <div style={{ textAlign: 'right', paddingRight: '20px' }}>
-        <Link to="/profile">Profile</Link>
-      </div>
+    <Header />
+      {/*}
      <ul className="App-header">
         <li>
           <Link to="/application">Application Page</Link>
         </li>
       </ul>
-      <div className="App">
-          <Footer />
-      </div></>
+    */}
+    <div className='main' style={mainStyle}>
+      <div className="logo">
+        <img src={SparkLogo} alt="Spark Logo"></img>
+      </div>
+      <div className="description" style={desStyle}>
+        <p>
+        <span style={{fontWeight: 'bold',fontSize: '2rem',}}>Spark </span>
+        {`is a nonprofit that offers
+        internship opportunities to undergraduate students. Our mission is to
+        provide a nurturing environment where students can learn valuable skills form mentors already in the 
+        field. Explore our intern tracks below!`}
+        </p>
+      </div>
+
+      <div className='intern-photo' style={photoStyle}>
+        <p>Photo here!</p>
+      </div>
+    </div>
+
+    <div className="Footer">
+        <Footer />
+    </div></>
 
     )
+}
+
+const mainStyle = {
+  padding: '2px',
+  display: 'flex',
+}
+
+
+const desStyle = {
+  flex: 1,
+  margin: '100px',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+const photoStyle = {
+  margin: '100px',
 }
