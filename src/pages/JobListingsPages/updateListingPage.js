4b366f596de829174b1db464c1c72d66c7293804
yanali000 <88128088+yanali000@users.noreply.github.com>
2023-08-07 17:01:39 -0700

    admin header

--- src/pages/JobListingsPages/updateListingPage.js
+++ src/pages/JobListingsPages/updateListingPage.js
@@ -1,16 +1,15 @@
 import { DataStore } from '@aws-amplify/datastore';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { JobListing } from '../../models';
-import {Stack, Button, Grid} from '@mui/material';
+import {Stack, Button, Grid, Box, TextField} from '@mui/material';
 import theme from './theme';
 import { ThemeProvider } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from 'dayjs';
 import { useLocation, useNavigate } from 'react-router-dom';
+import AdminHeader from '../../ui-components/AdminHeader';
 
 
 export default function UpdateListing(){
@@ -23,8 +22,14 @@ export default function UpdateListing(){
   const qualifications = useRef();
   const location = useRef();
   const deadline = useRef();
+
   
-    //will replace hard-coded id with id
+  const [titleError, setNameError] = useState('')
+  const [descriptionError, setDescriptionError] = useState('')
+  const [locationError, setLocationError] = useState('') 
+  const [qualificationsError, setQualificationsError] = useState('')
+  const [deadlineError, setDeadlineError] = useState('')
+  
   async function getListing(jobListing){
     const title = document.getElementById("title");
     title.value = jobListing.title;
@@ -50,20 +55,56 @@ export default function UpdateListing(){
   async function deleteListing(){
     const listingToDelete = await DataStore.query(JobListing, id);
     DataStore.delete(listingToDelete);
-    handleDelete();
+    handleBack();
   }
 
-  function handleDelete(){
+  function handleBack(){
     navigate('/admin');
   }
 
   async function updateListing() {
-    //if (jobListing === '') return
-    // if (title.current.value === '') return
-    // if (description.current.value === '') return
-    // if (deadline.current.value === '') return
-    // if (location.current.value === '') return
-    // if (qualifications.current.value === '') return
+    if (title.current.value === '') {
+      setNameError('Field Missing')
+      return
+    } else if (wordCounter(title.current.value) > 20) {
+      setNameError('Word limit is 20')
+      return
+    } else {
+      setNameError('')
+    }
+    if (description.current.value === '') {
+      setDescriptionError('Field Missing')
+      return
+    } else if (wordCounter(description.current.value) > 100) {
+      setDescriptionError('Word limit is 100')
+      return
+    } else {
+      setDescriptionError('')
+    }
+    if (location.current.value === '') {
+      setLocationError('Field Missing')
+      return
+    } else if (wordCounter(location.current.value) > 20) {
+      setLocationError('Word limit is 20')
+      return
+    } else {
+      setLocationError('')
+    }
+    if (qualifications.current.value === '') {
+      setQualificationsError('Field Missing')
+      return
+    } else if (wordCounter(qualifications.current.value) > 150) {
+      setQualificationsError('Word limit is 150')
+      return
+    } else {
+      setQualificationsError('')
+    }
+    if(deadline.current.value == null){
+      setDeadlineError('Field missing')
+      return
+    } else {
+      setDeadlineError('')
+    }
     const original = await DataStore.query(JobListing, id);
     const updatedListing = await DataStore.save(
     JobListing.copyOf(original, updated => {
@@ -73,47 +114,70 @@ export default function UpdateListing(){
       updated.location = location.current.value;
       updated.qualifications = qualifications.current.value;
     }));
+    handleBack();
+  }
+
+  function wordCounter(string) {
+    if (string === undefined) {
+      return 0;
+    }
+    return removeItemAll(string?.split(' '), "").length
+  }
+
+  function removeItemAll(arr, value) {
+    var i = 0;
+    while (i < arr.length) {
+      if (arr[i] === value) {
+        arr.splice(i, 1);
+      } else {
+        ++i;
+      }
+    }
+    return arr;
   }
 
   useEffect(() => {getListing(listing.state.listing)})
 
   return (
     <>
+    <AdminHeader/>
     <ThemeProvider theme={theme}>
-      <Box sx={{width: '100%', height: 189, mb: 4, backgroundColor: 'grey'}}></Box>
       <Stack sx={{justifyContent: 'center'}}>
         <Box sx={{width: 1290, height: 726, border: '1px solid', borderColor: 'secondary.main', borderRadius: 5}}>
         <Box sx={{width: 1290, height: 83, backgroundColor: 'secondary.main', textAlign: 'left', fontWeight: 500, fontSize: 25}}>
-          Job Listing
+          <div style={listingHeader}>Job Listing</div>
         </Box>
         <Grid container spacing={2} sx={{m: 2, justifyContent: 'space-between', width: 1290}}>
         <Grid item xs={5.5}>
           <Stack sx={{textAlign: 'left', justifyContent: 'space-between', fontSize: 20, fontWeight: 700, flexDirection: 'column', alignContent: 'flex-start'}}>
             Job Title
-            <TextField id="title" variant="outlined" inputRef={title} sx={{'& > :not(style)': {width: 519, height: 89}}} />
+            <TextField id="title" variant="outlined" inputRef={title} error={titleError !== ''} sx={{'& > :not(style)': {width: 519, height: 89}}} helperText={titleError} />
             <br></br>
             Job Description
-            <TextField id="description" multiline minRows={14} sx={{'& > :not(style)': {width: 519, height: 355, textAlign: 'left' }}} variant="outlined" inputRef={description} />
+            <TextField id="description" multiline minRows={14}  error={descriptionError !== ''} sx={{'& > :not(style)': {width: 519, height: 355, textAlign: 'left' }}} variant="outlined" inputRef={description} helperText={descriptionError} />
           </Stack>
         </Grid>
         <Grid item xs={5.5}>
           <Stack sx={{textAlign: 'left', justifyContent: 'space-between', fontSize: 20, fontWeight: 700, flexDirection: 'column', alignContent: 'flex-end'}}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
               Application Deadline
-              <DatePicker value={dayjs(listing.state.listing.deadline, "YYYY-MM-DD")} inputRef={deadline} sx={{'& > :not(style)': {width: 519, height: 89}}} />
+              <DatePicker value={dayjs(listing.state.listing.deadline, "YYYY-MM-DD")} error={deadlineError !== ''} inputRef={deadline} sx={{'& > :not(style)': {width: 519, height: 89}}} helperText={deadlineError}/>
             </LocalizationProvider>
             <br></br>
             Location
-            <TextField id="location" variant="outlined" inputRef={location} sx={{'& > :not(style)': { width: 519, height: 89 }}} />
+            <TextField id="location" variant="outlined" inputRef={location} error={locationError !== ''} sx={{'& > :not(style)': { width: 519, height: 89 }}} helperText={locationError}/>
             <br></br>
             Qualifications
-            <TextField id="qualifications" variant="outlined" inputRef={qualifications} sx={{'& > :not(style)': { width: 519, height: 209 }}} multiline minRows={8} />
+            <TextField id="qualifications" variant="outlined" error={qualificationsError !== ''} inputRef={qualifications} sx={{'& > :not(style)': { width: 519, height: 209 }}} multiline minRows={8} helperText={qualificationsError}/>
           </Stack>
         </Grid>
         </Grid>
         <Stack sx={{justifyContent: "center"}} spacing={2}>
           <Button variant="contained" size="large" onClick={updateListing} sx={{width: 144, height: 35}}>Update</Button>
           <Button variant="contained" size ="large" onClick={deleteListing} sx={{width: 144, height: 35}}>Delete</Button>
+        </Stack>
+        <Stack sx={{justifyContent: "center"}}>
+        <Button variant="contained" size ="large" onClick={handleBack} sx={{width: 144, height: 35}}>Back</Button>
         </Stack>  
         </Box>
       </Stack>
@@ -121,4 +185,11 @@ export default function UpdateListing(){
     </>
   )
 }
+
+const listingHeader = {
+  paddingLeft: 25,
+  paddingTop: 20
+}
+
+
 
